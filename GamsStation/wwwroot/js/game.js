82c52ce018a552b457a22a65c27667a6c3f00ca3
AlHareth alhyari <hdhareth@gmail.com>
2023-12-28 14:14:21 +0300

    final version #1

--- GamsStation/wwwroot/js/game.js
+++ GamsStation/wwwroot/js/game.js
@@ -17,7 +17,7 @@ function loadDataTable() {
                 data: 'id',
                 "render": function (data) {
                     return `<div class="w-75 btn-group" role="group">
-                     <a href="/admin/game/upsert?id=${data}" class="btn btn-primary mx-2"> <i class="bi bi-pencil-square"></i> Edit</a>               
+                     <a href="/admin/game/upsert?id=${data}" class="btn btn-info mx-2"> <i class="bi bi-pencil-square"></i> Edit</a>               
                      <a onClick=Delete('/admin/game/delete/${data}') class="btn btn-danger mx-2"> <i class="bi bi-trash-fill"></i> Delete</a>
                     </div>`
                 },
